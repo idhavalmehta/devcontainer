cb2dff5a6e5bd3323b7a9e7495800b5fb5cf5f6f
Dhaval Mehta <idhavalmehta@gmail.com>
2025-02-19 03:31:50 +0000

    install-scripts

---
+++ .devcontainer/install/install-node.sh
@@ -0,0 +1,22 @@
+apt-get update
+apt-get upgrade -y
+apt-get install -y curl
+
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+
+# Download and install Node.js:
+nvm install ${NVM_NODE_VERSION}
+
+# Verify the Node.js version:
+node -v # Should print "v22.14.0".
+nvm current # Should print "v22.14.0".
+
+# Verify npm version:
+npm -v # Should print "10.9.2".
+
+corepack enable yarn
+yarn -v
